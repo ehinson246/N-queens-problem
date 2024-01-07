cc705db6194fb98a6e2e78e92194b2490c8cb45a
ethan hinson <ethan.hinson@gmail.com>
2024-01-06 20:48:26 -0500

    v0.1.0 created working algorithm to fill board
    all that's left is to add diagonal vision
    (currently the queens only see horizontally and vertically)

---
+++ qeen-placement-algorithm.py
@@ -0,0 +1,170 @@
+#!/usr/bin/env python3
+
+N = 8
+
+EMPTY_SQUARE = 0
+
+ATTACKED_SQUARE = 1
+
+QUEEN_SQUARE = 2
+
+ATTACKED_QUEEN = ATTACKED_SQUARE + QUEEN_SQUARE
+
+# board_position = None
+
+def generate_empty_board_rows():
+    empty_rows = []
+    empty_row = []
+    while len(empty_rows) < N:
+        while len(empty_row) < N:
+            empty_row.append(EMPTY_SQUARE)
+        new_row = empty_row.copy()
+        empty_rows.append(new_row)
+    return empty_rows
+
+# from here on out, c is the actual column - 1
+def update_horizontal_vision(board_position, row, column):
+    for c in range(N):
+        square = board_position[row - 1][c]
+        if square & ATTACKED_SQUARE:
+            pass
+        else:
+            board_position[row - 1][c] += ATTACKED_SQUARE
+    board_position[row - 1][column - 1] = QUEEN_SQUARE
+
+# from here on out, r is the actual row - 1
+def update_vertical_vision(board_position, row, column):
+    for r in range(N):
+        square = board_position[r][column - 1]
+        if square & ATTACKED_SQUARE:
+            pass
+        else:
+            board_position[r][column - 1] += ATTACKED_SQUARE
+    board_position[row - 1][column - 1] = QUEEN_SQUARE
+
+
+
+
+
+def update_top_left_diagonal(board_position, row, column, squares_until_top, squares_until_left):
+    pass
+
+def update_top_right_diagonal(board_position, row, column, squares_until_top, squares_until_right):
+    pass
+
+def update_bottom_left_diagonal(board_position, row, column, squares_until_bottom, squares_until_left):
+    pass
+
+def update_bottom_right_diagonal(board_position, row, column, squares_until_bottom, squares_until_right):
+    pass
+
+def update_diagonal_vision(board_position, row, column):
+    squares_until_top = row - 1
+    squares_until_bottom = N - row
+    squares_until_left = column - 1
+    squares_until_right = N - column
+    update_top_left_diagonal(board_position, row, column, squares_until_top, squares_until_left)
+    update_top_right_diagonal(board_position, row, column, squares_until_top, squares_until_right)
+    update_bottom_left_diagonal(board_position, row, column, squares_until_bottom, squares_until_left)
+    update_bottom_right_diagonal(board_position, row, column, squares_until_bottom, squares_until_right)
+
+
+
+
+def update_queen_vision(board_position, row, column):
+    update_horizontal_vision(board_position, row, column)
+    update_vertical_vision(board_position, row, column)
+    update_diagonal_vision(board_position, row, column)
+
+def add_queen_to_board(board_position, row, column):
+    board_position[row - 1][column - 1] = QUEEN_SQUARE
+    update_queen_vision(board_position, row, column)
+
+
+
+board_position = generate_empty_board_rows()
+add_queen_to_board(board_position, 4, 4)
+for row in board_position:
+    print(row)
+
+
+
+
+def check_for_attacked_queens(board_position):
+    any_attacked_queens = False
+    for row in board_position:
+        attacked_queen_count = row.count(ATTACKED_QUEEN)
+        if attacked_queen_count > 0:
+            # print("\nAttacked queen found.\n")
+            any_attacked_queens = True
+            break
+    return any_attacked_queens
+
+def construct_trial_board(valid_squares_so_far):
+    trial_board_position = generate_empty_board_rows()
+    row = 0
+    for column in valid_squares_so_far:
+        row += 1
+        add_queen_to_board(trial_board_position, row, column)
+    return trial_board_position
+
+def find_solution():
+    # the index for valid squares corresponds with the row - 1, and the value is the column
+    valid_squares_so_far = []
+    # failed_trial_columns = []
+    # next_backtrack_trial_column = None
+    current_row = 1
+    trial_column = 1
+
+    while current_row <= N:
+
+        trial_board_position = construct_trial_board(valid_squares_so_far)
+        # for row in trial_board_position:
+        #     print(row)
+
+        trial_square = trial_board_position[current_row - 1][trial_column - 1]
+        # print(trial_square)
+
+        while trial_square > 0:
+            trial_column += 1
+            if trial_column > N:
+                trial_column = valid_squares_so_far[-1]
+                valid_squares_so_far.pop(-1)
+                current_row -= 1
+                if current_row == 0:
+                    return None
+            trial_square = trial_board_position[current_row - 1][trial_column - 1]   
+        # print(trial_column)
+
+        add_queen_to_board(trial_board_position, current_row, trial_column)
+        # for row in trial_board_position:
+        #     print(row)
+        any_attacked_queens = check_for_attacked_queens(trial_board_position)
+
+        if any_attacked_queens:
+            # print("Attacked queen!")
+            trial_column += 1
+            if trial_column > N:
+                trial_column = valid_squares_so_far[-1]
+                valid_squares_so_far.pop(-1)
+                current_row -= 1
+                if current_row == 0:
+                    return None
+        else:
+            # print("No attacked queens.")
+            valid_squares_so_far.append(trial_column)
+            trial_column = 1
+            current_row += 1
+    
+    solution = construct_trial_board(valid_squares_so_far)
+    return solution
+
+def print_solution():
+    solution = find_solution()
+    if solution is None:
+        print("\nNo solution.\n")
+    else:
+        for row in solution:
+            print(row)
+
+# print_solution()
